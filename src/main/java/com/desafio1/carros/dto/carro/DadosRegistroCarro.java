1321257804f102de95861220c28c69f20ccc52ae
MaiiZuko <maiaramzucco@gmail.com>
2023-12-19 16:20:33 -0300

    update

--- src/main/java/com/desafio1/carros/dto/carro/DadosRegistroCarro.java
+++ src/main/java/com/desafio1/carros/dto/carro/DadosRegistroCarro.java
@@ -1,15 +1,23 @@
 package com.desafio1.carros.dto.carro;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record DadosRegistroCarro(
     
+
     Long chassis_id,
 
+    @NotBlank
     String model,
 
+    @NotNull
     String brand,
 
+    @NotBlank
     String color,
 
+    @NotBlank
     String fabricationYear) {
 
 }

11f50e03196dd3eb4779db1404ac55f7c454fb6c
MaiiZuko <maiaramzucco@gmail.com>
2023-12-19 14:26:48 -0300

    Projeto desafio

---
+++ src/main/java/com/desafio1/carros/CarrosApplication.java
@@ -0,0 +1,10 @@
+package com.desafio1.carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class CarrosApplication {
+
+
+}
---
+++ src/test/java/com/desafio1/carros/CarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio1.carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
